23cc949e2593b6f8323b06437a7071d96deecd90
Manikandan Swaminathan <mswami2@illinois.edu>
2021-03-31 19:08:57 -0500

    added emotions graph for Starbucks

--- shiny/app.R
+++ shiny/app.R
@@ -8,6 +8,7 @@ library(plotly)
 library(hrbrthemes)
 library(shinydashboard)
 require(maps)
+library(shinyjs)
 
 #Create data for the scatter plot
 datasets <- read_excel("data/sentiments.xlsx")
@@ -71,6 +72,22 @@ name_data_change <- geography_named %>%
 geography_data_with_map <- world_map_named %>%
   left_join(geography_named, by=c("region" = "countries"))
 
+#Create data for the emotions plot
+emotions <- read_excel("data/emotions.xlsx")
+
+emotion_pivot <- emotions %>%
+  pivot_longer(!days, names_to = "Emotion", values_to = "Count") %>%
+  mutate(Day=days)
+
+
+summaryTibbleEmotions <- datasets %>%
+  summarize(mindate = min(days), maxdate = max(days)) %>%
+  head()
+
+
+minPossibleDateEmotion <- summaryTibbleEmotions$mindate
+maxPossibleDateEmotion <- summaryTibbleEmotions$maxdate
+
 ui <- dashboardPage(
   dashboardHeader(disable=TRUE),
   dashboardSidebar(),
@@ -107,14 +124,44 @@ ui <- dashboardPage(
                    )
                  )
         ),
+        tabPanel("Emotions over Time", 
+                 fluidRow(useShinyjs(),
+                   # Boxes need to be put in a row (or column)
+                   box(background="orange",width=4,
+                       selectInput("type", "Graph to Display:", 
+                                   c("Bar" = "Bar",
+                                     "Line" = "Line"), selected="Line"),
+                       selectInput(
+                         inputId = "emotionTypes", label = "Emotions to include:",
+                         choices = c("anger", "sadness", "disgust",
+                                     "fear", "joy", "surprise"),
+                         multiple=TRUE,
+                         selected = "joy", width = "350px"
+                       ),
+                       
+                       dateRangeInput('dateRangeEmotion',
+                                      label = 'Date range input: yyyy-mm-dd',
+                                      start = minPossibleDateEmotion, end = maxPossibleDateEmotion,
+                                      min = minPossibleDateEmotion, max = maxPossibleDateEmotion
+                       ),
+                   ),
+                   mainPanel(
+                     # Output: Formatted text for caption ----
+                     h3("Emotions over Time"),
+                     
+                     # Output: Plot of the requested variable against mpg ----
+                     plotlyOutput("emotionPlot")
+                   )
+                 )
+        ),
         tabPanel("World Tweet Volume",
-          mainPanel(
-            # Output: Formatted text for caption ----
-            h3("World Tweet Volume"),
-            
-            # Output: Plot of the requested variable against mpg ----
-            plotlyOutput("geographyPlot", width="800px")
-          )
+                 mainPanel(
+                   # Output: Formatted text for caption ----
+                   h3("World Tweet Volume"),
+                   
+                   # Output: Plot of the requested variable against mpg ----
+                   plotlyOutput("geographyPlot", width="800px")
+                 )
         )
       )
     )
@@ -134,7 +181,15 @@ server <- function(input, output, session) {
     formulaText()
   })
   
-  
+  observeEvent(input$type, {
+    
+    if("Line" %in% input$type) {
+      shinyjs::show(id = "emotionTypes")
+    }
+    else {
+      shinyjs::hide(id = "emotionTypes")
+    }
+  })
   
   output$sentimentPlot <- renderPlotly({
     tempData <- datasets %>%
@@ -197,8 +252,40 @@ server <- function(input, output, session) {
       scale_fill_viridis_c(option = "C")+
       theme_void() +
       labs(fill="Tweet Volume (Power of 10)")
-    plot <- ggplotly(plot, tooltip = c("text")) 
+    axisData <- list(
+      title = "",
+      zeroline = FALSE,
+      showline = FALSE,
+      showticklabels = FALSE,
+      showgrid = FALSE
+    )
+    plot <- ggplotly(plot, tooltip = c("text")) %>%
+      layout(xaxis = axisData, yaxis = axisData)
 
+    plot
+  })
+  
+  output$emotionPlot <- renderPlotly({
+    
+    tempData <- emotion_pivot %>%
+      filter(Day >= ymd(input$dateRangeEmotion[1]), Day <= ymd(input$dateRangeEmotion[2]))
+    if ("Line" %in% input$type) {
+      tempData <- tempData %>%
+        filter(str_detect(Emotion, paste(input$emotionTypes, collapse="|")))
+      plot = ggplot(data=tempData)
+      
+      plot <- plot + 
+        geom_line(mapping=aes(x=Day, y=Count)) +
+        facet_wrap(~ Emotion, nrow=3) +
+        labs(x="Date", y = "Frequency (Raw count)")
+    }
+    else {
+      plot = ggplot(data=tempData)
+      plot <- plot + 
+        geom_col(position="fill", mapping=aes(x=Day, y=Count, fill=Emotion)) + 
+        labs(x="Date", y = "Ratio of emotions")
+    }
+    plot <- ggplotly(plot, dynamicTicks=TRUE)
     plot
   })
 }
